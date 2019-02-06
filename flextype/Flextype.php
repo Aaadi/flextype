d9b97ad0ec6edff61183770e41de2db010ab7771
Awilum <awilum@msn.com>
2019-02-06 21:24:55 +0300

    Flextype Core: working on entries mock data and methods

--- flextype/Flextype.php
+++ flextype/Flextype.php
@@ -14,5 +14,40 @@ namespace Flextype;
 
 
 class Flextype {
-    
+
+    public static $entries = [
+        'home' => [
+            'title' => 'Home',
+            'content' => 'Home content'
+        ],
+        'about' => [
+            'title' => 'About',
+            'content' => 'About content'
+        ],
+    ];
+
+    public static function getEntry($entry)
+    {
+        return Flextype::$entries[$entry];
+    }
+
+    public static function getEntryField($entry, $field)
+    {
+        return Flextype::$entries[$entry][$field];
+    }
+
+    public static function createEntry($entry, $data)
+    {
+        Flextype::$entries[$entry] = $data;
+    }
+
+    public static function updateEntry($entry, $data)
+    {
+        Flextype::$entries[$entry] = $data;
+    }
+
+    public static function deleteEntry($entry)
+    {
+        unset(Flextype::$entries[$entry]);
+    }
 }
